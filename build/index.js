89f183c8ba51ec01623b6dec8d16e36c36fad206
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-03-27 18:37:48 +0200

    exercise-1 express and cors configuration, first endpoint

--- build/index.js
+++ build/index.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 const types_1 = require("./types");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+app.get("/ping", (req, res) => {
+    res.send("Pong!");
+});
 console.log((0, database_1.createUser)("004", "beltrano@email.com", "beltrano99"));
 console.log((0, database_1.getAllUsers)());
 console.log((0, database_1.createProduct)("p004", "Monitor HD", 800, types_1.PRODUCT_CATEGORIES.ELECTRONICS));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { users, products, purchases, getAllUsers, createUser, createProduct, getAllProducts, getProductById, queryProductByName } from "./database";
 import { PRODUCT_CATEGORIES } from "./types"
+import express, { Request, Response} from 'express'
+import cors from 'cors';
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get("/ping", (req: Request, res: Response) => {
+    res.send("Pong!")
+})
 
 
 // console.log("Usuarios", users, "Produtos", products, "Compras", purchases)
