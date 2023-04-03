ad282ec30cde0654ca80fc7d006e9b9c67f22588
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-04-03 18:49:43 +0200

    creating producttable - exercise 3

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -19,3 +19,22 @@ VALUES
     ("003", "pedro@email.com", "pedro123");
 
 -----------TABELA PRODUTOS
+
+CREATE TABLE products (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    category TEXT NOT NULL
+);
+
+DROP TABLE products;
+
+SELECT * FROM products;
+
+INSERT INTO products (id, name, price, category)
+VALUES
+    ("p001","vaso", 50, "decoração"),
+    ("p002","tv", 1.500, "eletrodomésticos"),
+    ("p003","aspirador", 300, "eletrodomésticos"),
+    ("p004","sofá", 2.500, "móveis"),
+    ("p005","quadro", 150, "decoração");
