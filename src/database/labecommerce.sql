6a3a700d5c129fc5975f025eb528b46d59f37a7f
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-04-03 18:24:41 +0200

    creating users table - exercise -2

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,1 +1,21 @@
 -- Active: 1680538089515@@127.0.0.1@3306
+
+-----------TABELA USUÁRIOS
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+SELECT * FROM users;
+
+DROP TABLE users;
+
+INSERT INTO users (id, email, password)
+VALUES
+    ("001", "maria@email.com", "maria123"),
+    ("002", "joana@email.com", "joana123"),
+    ("003", "pedro@email.com", "pedro123");
+
+-----------TABELA PRODUTOS
